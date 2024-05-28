9087348f707a75227d7c6185003ddc612223ab93
Jonas Wahl <jonasjorge97@gmail.com>
2024-05-28 16:03:33 +0100

    Add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,27 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-
-# Destroy all existing records to start from a clean state
-Prop.destroy_all
+puts 'Destroying all existing records to start from a clean state'
 User.destroy_all
+Prop.destroy_all
 
-# Create a new user
-@batman = User.create!(username: "test1", email: "test1@test.com", password: '123456')
+puts 'Creating 5 users'
+@propper_gary = User.create!(username: "PropperGary123", email: "ppgary@example.com", password: "password")
+@kinophile = User.create!(username: "kinophile", email: "kino@example.com", password: "tarkovismyidol")
+@weeb215 = User.create!(username: "weeb215", email: "weeb@example.com", password: "iloveanime")
+@jester_john = User.create!(username: "Jester_John", email: "johnjester4@example.com", password: "lol")
+@jjwahl = User.create!(username: "jjwahl", email: "jonas@example.com", password: "password")
 
-# Create a new prop
-@batmobile = Prop.create!(name: "batmobile", user: @batman)
+puts 'Creating 10 movie props'
+@prop1 = Prop.create!(name: "Inception Totem", user: @jester_john)
+@prop2 = Prop.create!(name: "Harry Potter Wand", user: @propper_gary)
+@prop3 = Prop.create!(name: "Guts' Giant Sword", user: @weeb215)
+@prop4 = Prop.create!(name: "The One Ring", user: @propper_gary)
+@prop5 = Prop.create!(name: "Batmobile", user: @propper_gary)
+@prop6 = Prop.create!(name: "Coop's Damn Good Cup of Coffee", user: @jjwahl)
+@prop7 = Prop.create!(name: "Pulp Fiction Briefcase", user: @kinophile)
+@prop8 = Prop.create!(name: "Fight Club Soap", user: @jester_john)
+@prop9 = Prop.create!(name: "Paterson's Notebook", user: @jjwahl)
+@prop10 = Prop.create!(name: "Tarkowsky's Mirror", user: @kinophile)
 
 puts 'Done!'
