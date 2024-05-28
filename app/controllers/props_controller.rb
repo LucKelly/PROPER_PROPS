4b559df6d0368c29cde49832f59f84e681c80edf
Luc Kelly <luckelly@live.co.uk>
2024-05-28 17:14:22 +0100

    added show method and view file

--- app/controllers/props_controller.rb
+++ app/controllers/props_controller.rb
@@ -14,7 +14,11 @@ class PropsController < ApplicationController
   def index
     @props = Prop.all
   end
-    
+
+  def show
+    @prop = Prop.find(params[:id])
+  end
+
   def new
     @prop = Prop.new
   end
