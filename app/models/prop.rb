c9bbdf9e58436064aaaf7865a3368300581a2a6c
aureoricardo <aureoricardo10@hotmail.com>
2024-05-28 14:26:22 +0100

    add validations

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -1,4 +1,7 @@
 class Prop < ApplicationRecord
   belongs_to :user
   has_many :users, through: :bookings
+  validates :name, uniqueness: true
+  validates :description, :price, presence: true
+  validates :category, inclusion: { in: %w[cars fantasy series movies weapons clothes real-life stuff action drama comedy]}
 end
